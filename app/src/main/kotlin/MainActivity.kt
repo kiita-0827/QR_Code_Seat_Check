a7a6dab4729b8afb5c78085327cc3625d8190ad3
yusuke higashi <higaora.com@gmail.com>
2023-06-23 13:42:05 +0900

    test

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,62 +1,26 @@
-import android.Manifest
-import android.content.pm.PackageManager
 import android.hardware.Camera
-import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var camera: Camera
-    private lateinit var cameraPreview: CameraPreview
-
-    companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 200
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // カメラのパーミッションをリクエストする
-        requestCameraPermission()
-    }
-
-    private fun requestCameraPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_REQUEST_CODE)
-        } else {
-            initializeCamera()
+    private fun safeCameraOpen(id: Int): Boolean {
+        return try {
+            releaseCameraAndPreview()
+            mCamera = Camera.open(id)
+            true
+        } catch (e: Exception) {
+            Log.e(getString(R.string.app_name), "failed to open Camera")
+            e.printStackTrace()
+            false
         }
     }
 
-    private fun initializeCamera() {
-        camera = Camera.open()
-
-        val previewLayout = findViewById<FrameLayout>(R.id.camera_preview)
-        cameraPreview = CameraPreview(this, camera)
-        previewLayout.addView(cameraPreview)
-    }
-
-    private fun startCamera() {
-        camera.startPreview()
-    }
-
-    private fun stopCamera() {
-        camera.stopPreview()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            initializeCamera()
+    private fun releaseCameraAndPreview() {
+        preview?.setCamera(null)
+        mCamera?.also { camera ->
+            camera.release()
+            mCamera = null
         }
     }
 
-    override fun onPause() {
-        super.onPause()
-        stopCamera()
-        camera.release()
-    }
 }

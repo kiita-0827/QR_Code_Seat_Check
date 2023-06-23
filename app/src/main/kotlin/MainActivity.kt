e69cb90bc7420bb451a71daa835f74888738bd2c
yusuke higashi <higaora.com@gmail.com>
2023-06-23 16:07:14 +0900

    test

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,28 +1,79 @@
-import android.hardware.Camera
-import androidx.appcompat.app.AppCompatActivity
+package jp.co.casareal.camerabasicsample
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val CAMERA_REQUEST_CODE = 1
+        const val CAMERA_PERMISSION_REQUEST_CODE = 2
+    }
 
-    private lateinit var mCamera: Camera
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
-    private fun safeCameraOpen(id: Int): Boolean {
-        return try {
-            releaseCameraAndPreview()
-            mCamera = Camera.open(id)
-            true
-        } catch (e: Exception) {
-            Log.e(getString(R.string.app_name), "failed to open Camera")
-            e.printStackTrace()
-            false
+    override fun onResume() {
+        super.onResume()
+
+        btnLaunchCamera.setOnClickListener {
+            // カメラ機能を実装したアプリが存在するかチェック
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).resolveActivity(packageManager)?.let {
+                if (checkCameraPermission()) {
+                    takePicture()
+                } else {
+                    grantCameraPermission()
+                }
+            } ?: Toast.makeText(this, "カメラを扱うアプリがありません", Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun releaseCameraAndPreview() {
-        preview?.setCamera(null)
-        mCamera?.also { camera ->
-            camera.release()
-            mCamera = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val image = data?.extras?.get("data")?.let {
+                cameraImage.setImageBitmap(it as Bitmap)
+            }
         }
     }
 
+    private fun takePicture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            addCategory(Intent.CATEGORY_DEFAULT)
+        }
+
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
+    private fun checkCameraPermission() = PackageManager.PERMISSION_GRANTED ==
+            ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+
+
+    private fun grantCameraPermission() =
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE)
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePicture()
+            }
+        }
+    }
 }
